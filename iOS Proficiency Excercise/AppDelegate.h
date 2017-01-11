f2b879e44e683a5d729e278f65b5229b744deb22
Sanjeet Kumar <sanjeet.kumar@infosys.com>
2017-01-11 17:11:05 +0530

    Initial Commit

---
+++ iOS Proficiency Excercise/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Proficiency Excercise
+//
+//  Created by Sanjeet on 1/11/17.
+//  Copyright © 2017 Sanjeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS Proficiency Excercise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOS Proficiency Excercise
+//
+//  Created by Sanjeet on 1/11/17.
+//  Copyright © 2017 Sanjeet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
